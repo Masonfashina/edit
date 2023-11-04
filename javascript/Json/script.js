e0e3bf51721e0fa68a00cdadeb88c157886d3c91
Mason <Masonfashina@icloud.com>
2023-11-04 13:01:07 +0100

    new folders

--- javascript/Json/script.js
+++ javascript/Json/script.js
@@ -1,7 +1,9 @@
 const post = {
-    id: 1,
-    title: 'Blog post',
-    body: 'This is the body'
-}
+  id: 1,
+  title: "Blog post",
+  body: "This is the body",
+};
 
-console.log(post)
+// Convert to json
+const str = JSON.stringify(post);
+console.log(str);
