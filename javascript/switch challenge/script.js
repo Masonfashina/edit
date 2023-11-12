dccc8ef6d0d7f4441f70e53db39325ac5ea7227a
Mason <Masonfashina@icloud.com>
2023-11-12 16:42:58 +0100

    ternary operator

--- javascript/switch challenge/script.js
+++ javascript/switch challenge/script.js
@@ -1,23 +1,23 @@
-function calculator (num1,num2,operator){
-    switch (operator){
-        case '+':
-            result = num1 + num2
-            break
-        case '-':
-            result = num1 - num2
-            break
-        case '*':
-            result = num1 * num2
-            break
-        case '/':
-            result = num1 / num2
-            break
-        default: 
-        result ='Invalid Syntax'
-    }
-    
-    console.log(result)
-     return(result)
+function calculator(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      result = num1 / num2;
+      break;
+    default:
+      result = "Invalid Syntax";
+  }
+
+  console.log(result);
+  return result;
 }
 
-calculator(5,2, '*')
+calculator(5, 2, "/");
---
+++ javascript/ternary operator/script.js
@@ -0,0 +1,17 @@
+//normal operator
+
+const age = 19
+
+if (age >= 18){
+    console.log('You can Vote!');}
+    else{
+        console.log('You can not Vote')
+    }
+
+// Ternary operator
+
+age >= 18 ? console.log('You can Vote!') : console.log('you can not Vote')
+
+const canVote = age >= 18 ? true : false
+
+console.log(canVote)
