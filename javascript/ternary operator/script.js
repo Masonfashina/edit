926e596ecda3b32c3bc2b41f86e435d45edb52cc
Mason <Masonfashina@icloud.com>
2023-11-12 16:48:45 +0100

    added auth logic

--- javascript/ternary operator/script.js
+++ javascript/ternary operator/script.js
@@ -1,17 +1,32 @@
 //normal operator
 
-const age = 19
+const age = 19;
 
-if (age >= 18){
-    console.log('You can Vote!');}
-    else{
-        console.log('You can not Vote')
-    }
+if (age >= 18) {
+  console.log("You can Vote!");
+} else {
+  console.log("You can not Vote");
+}
 
 // Ternary operator
 
-age >= 18 ? console.log('You can Vote!') : console.log('you can not Vote')
+age >= 18 ? console.log("You can Vote!") : console.log("you can not Vote");
 
-const canVote = age >= 18 ? true : false
+const canVote = age >= 18 ? true : false;
 
-console.log(canVote)
+console.log(canVote);
+
+//multiple statements
+
+const auth = true;
+let redirect;
+
+if (auth) {
+  alert("welcome to the dashboard");
+  redirect = "/dashboard";
+} else {
+  alert("access denied mf");
+  redirect = "/login";
+}
+
+console.log(redirect);
