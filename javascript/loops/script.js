fa0917a5e90fa252f255ef59394baac61948367a
Mason <Masonfashina@icloud.com>
2023-11-16 01:49:33 +0100

    added more loop concepts

---
+++ javascript/loops/script.js
@@ -0,0 +1,15 @@
+// for (let i = 0; i <= 100; i++) {
+//   console.log(i);
+//   if (i === 15) {
+//     console.log("breaking...");
+//     break;
+//   }
+// }
+
+for (let i = 0; i <= 20; i++) {
+  if (i === 13) {
+    console.log("skipping the spooky number...");
+    continue;
+  }
+  console.log(i);
+}
