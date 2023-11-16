caeb290e766f13b5483481ce7e1eb565af30e281
Mason <Masonfashina@icloud.com>
2023-11-16 02:21:51 +0100

    did array loops

--- javascript/for loop/script.js
+++ javascript/for loop/script.js
@@ -1,6 +1,13 @@
 let i = 0;
 
-while(i <=20){
-    console.log('Number ' + i);
-    i++;
+// while(i <=20){
+//     console.log('Number ' + i);
+//     i++;
+// }
+
+const arr = [10, 20, 30, 40, 50];
+
+while (i < arr.length) {
+  console.log(arr[i]);
+  i++;
 }
