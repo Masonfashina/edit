e29ead8904b1ad6c969d3957b6d5fa42debbf1c1
Mason <Masonfashina@icloud.com>
2023-11-18 00:06:57 +0100

    wow!

--- javascript/array.reduce/script.js
+++ javascript/array.reduce/script.js
@@ -3,3 +3,14 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const sum = numbers.reduce((accumulator,currentValue,)=> accumulator + currentValue, 0 )
 
 console.log(sum)
+
+
+const cart =[
+    {id:1, name: 'Product 1', price: 130},
+    {id:2, name: 'Product 2', price: 150},
+    {id:3, name: 'Product 3', price: 175},
+]
+
+const finalPrice =cart.reduce((acc,product) => acc + product.price,0)
+
+console.log('your total bill is ' + '$' + finalPrice)
