15783824692716ce5f649c764bc351df329fc718
Mason <Masonfashina@icloud.com>
2023-11-17 01:19:37 +0100

    yessss

--- javascript/for each/script.js
+++ javascript/for each/script.js
@@ -1,3 +1,14 @@
-const socials = ['Twitter', 'Linkedin', 'Instagram', 'Snapchat']
+const socials = ["Twitter", "Linkedin", "Instagram", "Snapchat"];
 
-socials.forEach((item) => console.log(item))
+socials.forEach((item, index, arr) => console.log(`${index} - ${item}`));
+
+
+
+const socialObjs = [
+    {name:'Twitter', url: 'https://www.twitter.com'},
+    {name:'Instagram', url: 'https://www.instagram.com'},
+    {name:'Linkedin', url: 'https://www.linkedin.com'},
+    {name:'Snapchat', url: 'https://www.snapchat.com'},
+]
+
+socialObjs.forEach((item) => console.log(item))
