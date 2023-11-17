254603cf84892a90a3d3e140fba1c4ce4cad1a78
Mason <Masonfashina@icloud.com>
2023-11-17 02:05:09 +0100

    killing it

--- javascript/array.filter/script.js
+++ javascript/array.filter/script.js
@@ -1,5 +1,29 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const evenNumbers = numbers.filter((number) => number % 2 === 0);
+// const evenNumbers = numbers.filter((number) => number % 2 === 0);
 
-console.log(evenNumbers);
+// console.log(evenNumbers);
+
+
+const companies = [
+    {name:'company one', category: 'finance', start: 1981, end: 2000},
+    {name:'company two', category: 'auto', start: 1982, end: 2002},
+    {name:'company three', category: 'retail', start: 1998, end: 2008},
+    {name:'company four', category: 'technology', start: 1989, end: 2006},
+    {name:'company five', category: 'finance', start: 2009, end: 2022},
+    {name:'company six', category: 'finance', start: 1996, end: 2008},
+    {name:'company seven', category: 'auto', start: 1982, end: 2009},
+    {name:'company eight', category: 'retail', start: 1988, end: 2004},
+    {name:'company nine', category: 'technology', start: 2017, end: 2020}
+]
+const retailCompanies = companies.filter((companies) => companies.category ==='retail')
+
+console.log(retailCompanies)
+
+const autoCompanies = companies.filter((companies) => companies.category === 'auto')
+
+console.log(autoCompanies)
+
+const techCompanies = companies.filter((companies) => companies.category === 'technology')
+
+console.log(techCompanies)
