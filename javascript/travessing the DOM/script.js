38ee1cc3b9d39fbf929a76d7210c15166b7d6999
Mason <Masonfashina@icloud.com>
2023-11-19 15:02:07 +0100

    travessing the DOM1

---
+++ javascript/travessing the DOM/script.js
@@ -0,0 +1,10 @@
+let output;
+
+//get child elements
+
+const parent = document.querySelector(".parent");
+
+output = parent.children;
+output = parent.children[1].innerText;
+
+console.log(output);
