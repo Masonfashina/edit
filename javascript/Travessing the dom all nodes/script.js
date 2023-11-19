e20cdde19df07793a8445b82ea58404b84536a98
Mason <Masonfashina@icloud.com>
2023-11-19 19:06:55 +0100

    learned to access parent from child

--- javascript/Travessing the dom all nodes/script.js
+++ javascript/Travessing the dom all nodes/script.js
@@ -1,8 +1,19 @@
 let output;
 
-const parent = document.querySelector('.parent');
+const parent = document.querySelector(".parent");
 
 output = parent.childNodes;
+output = parent.childNodes[3].textContent;
+output = parent.childNodes[3].outerHTML;
 
 
-console.log(output)
+const child = document.querySelector('.child')
+
+output = child.parentNode
+
+child.parentNode.style.backgroundColor = '#ccc';
+child.parentNode.style.padding = '12px';
+child.parentNode.style.borderRadius = '12px';
+
+console.log(output);
+
