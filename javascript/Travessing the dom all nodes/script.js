af8f91fb200410992ec0851f9bed82505f344d18
Mason <Masonfashina@icloud.com>
2023-11-19 15:58:54 +0100

    child nodes

---
+++ javascript/Travessing the dom all nodes/script.js
@@ -0,0 +1,8 @@
+let output;
+
+const parent = document.querySelector('.parent');
+
+output = parent.childNodes;
+
+
+console.log(output)
--- javascript/travessing the DOM/script.js
+++ javascript/travessing the DOM/script.js
@@ -7,4 +7,35 @@ const parent = document.querySelector(".parent");
 output = parent.children;
 output = parent.children[1].innerText;
 
+
+parent.children[1].innerText = 'child Two'
+parent.children[1].style.color = 'palevioletred'
+
+parent.firstElementChild.innerText = 'Child One'
+parent.firstElementChild.style.color = 'blue'
+parent.lastElementChild.innerText = 'Child Three'
+parent.lastElementChild.style.color = 'grey'
+
+
+//get parent element from child
+
+const child = document.querySelector('.child')
+
+output = child.parentElement
+
+child.parentElement.style.border = '1px solid #ccc'
+child.parentElement.style.padding = '10px'
+
+//sibling element
+
+
+const secondItem = document.querySelector('.child:nth-child(2)')
+
+output = secondItem;
+output = secondItem.nextElementSibling
+
+
+secondItem.nextElementSibling.style.color = 'green'
+secondItem.previousElementSibling.style.color = 'yellow'
+
 console.log(output);
