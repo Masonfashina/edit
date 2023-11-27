c228dd7f08ea34ba5ce1d323437f3dc55838c818
Mason <Masonfashina@icloud.com>
2023-11-27 23:23:34 +0100

    finally added shopping list

--- javascript/event-keycodes-start/script.js
+++ javascript/event-keycodes-start/script.js
@@ -1,22 +1,39 @@
 //method 1
 
-window.addEventListener("keydown", (e) => {
+// window.addEventListener("keydown", (e) => {
+//   const insert = document.getElementById("insert");
+
+//   insert.innerHTML = `
+//     <div class="key">
+//           ${e.key ===' ' ? 'space' : e.key}
+//           <small>e.key</small>
+//         </div>
+
+//         <div class="key">
+//           ${e.keyCode}
+//           <small>e.keyCode</small>
+//         </div>
+
+//         <div class="key">
+//           ${e.code}
+//           <small>e.code</small>
+//         </div>
+//     `;
+// });
+
+//method 2
+
+function showKeyCodes(e) {
   const insert = document.getElementById("insert");
 
-  insert.innerHTML = `
-    <div class="key">
-          ${e.key ===' ' ? 'space' : e.key}
-          <small>e.key</small>
-        </div>
+  const keyCodes = {
+    "e.key": e.key === " " ? "space" : e.key,
+    "e.keyCode": e.keyCode,
+    "e.code": e.code,
+  };
+  console.log(keyCodes);
+}
 
-        <div class="key">
-          ${e.keyCode}
-          <small>e.keyCode</small>
-        </div>
+window.addEventListener("keydown", showKeyCodes);
 
-        <div class="key">
-          ${e.code}
-          <small>e.code</small>
-        </div>
-    `;
-});
+//method 2 is unfinished, i may return...
