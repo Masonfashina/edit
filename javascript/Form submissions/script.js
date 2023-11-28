476986affc32b23474a1ea92f51844d42f5966b0
Mason <Masonfashina@icloud.com>
2023-11-28 01:35:00 +0100

    learned form validation

---
+++ javascript/Form submissions/script.js
@@ -0,0 +1,18 @@
+const form = document.getElementById("item-form");
+
+function onSubmit(e) {
+  e.preventDefault();
+  
+  const item = document.getElementById('item-input').value;
+  const priority = document.getElementById('priority-input').value;
+
+
+  if (item === '' || priority === '0') {
+    alert('please fill in all fields')
+
+    return
+  }
+  console.log(item,priority)
+}
+
+form.addEventListener("submit", onSubmit);
