eb9fbe3a136c8f83b9d21ea7a62f803f803b570e
Mason <Masonfashina@icloud.com>
2023-11-30 20:36:01 +0100

    learned about multiple event handling

--- javascript/Event delegation & multiple events/script.js
+++ javascript/Event delegation & multiple events/script.js
@@ -1,7 +1,22 @@
 const listItems = document.querySelectorAll("li");
+const list = document.querySelector("ul");
 
-listItems.forEach((item) => {
-  item.addEventListener("click", (e) => {
-    e.target.remove();
-  });
-});
+// listItems.forEach((item) => {
+//   item.addEventListener("click", (e) => {
+//     e.target.remove();
+//   });
+// });
+
+
+//event delegation
+
+list.addEventListener('click', (e) =>{
+    if(e.target.tagName === 'LI'){
+        e.target.remove()
+    }
+})
+list.addEventListener('mouseover', (e) =>{
+    if(e.target.tagName === 'LI'){
+        e.target.style.color = 'Green'
+    }
+})
