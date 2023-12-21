300fcff132e4e4d6a59874f2244dcb93ffeb0fa3
Mason <Masonfashina@icloud.com>
2023-12-21 19:18:29 +0100

    error handling init

---
+++ javascript/Error handling with Async & Await/script.js
@@ -0,0 +1,13 @@
+const getUsers = async () =>{
+    try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/users')
+
+        const data = await res.json()
+
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getUsers()
--- javascript/Try... Catch/script.js
+++ javascript/Try... Catch/script.js
@@ -1,9 +1,20 @@
-try{
-    console.log(x)
+// try {
+//   console.log(x);
+// } catch (error) {
+//   console.log("Error: " + error);
+// }
 
+function double (number){
+    if(isNaN(number)){
+        throw new Error(number  + ' is not a number')
+    }
 
-}catch(error){
-    console.log('Error: ' + error)
+    return number * 2;
 }
 
-
+try{
+const y = double('hello')
+console.log(y)
+}catch(error){
+console.log(error)
+}
