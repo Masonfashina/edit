7d43b01ea3c94f38d8f8bb849508ebfaf0981d40
Mason <Masonfashina@icloud.com>
2023-12-21 18:54:27 +0100

    async await done

--- javascript/Async & Await/script.js
+++ javascript/Async & Await/script.js
@@ -8,4 +8,12 @@ async function getPromise() {
   const response = await promise;
   console.log(response);
 }
-getPromise();
+// getPromise();
+
+const getPosts = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+  console.log(data);
+};
+
+getPosts()
