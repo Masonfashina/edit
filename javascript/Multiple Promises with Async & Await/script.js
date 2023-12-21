46d83c7fb3f89a02f2587f67af201b577082f181
Mason <Masonfashina@icloud.com>
2023-12-21 19:46:44 +0100

    async edit complete

--- javascript/Multiple Promises with Async & Await/script.js
+++ javascript/Multiple Promises with Async & Await/script.js
@@ -18,16 +18,25 @@ function getData(endpoint) {
     }, Math.floor(Math.random() * 3000) + 1000);
   });
 }
-getData("./movies.json")
-  .then((movies) => {
-    console.log(movies);
-    return getData("./actors.json");
-  })
-  .then((actors) => {
-    console.log(actors);
-    return getData("./directors.json");
-  })
-  .then((directors) => {
-    console.log(directors);
-  })
-  .catch((error) => console.log(error));
+// getData("./movies.json")
+//   .then((movies) => {
+//     console.log(movies);
+//     return getData("./actors.json");
+//   })
+//   .then((actors) => {
+//     console.log(actors);
+//     return getData("./directors.json");
+//   })
+//   .then((directors) => {
+//     console.log(directors);
+//   })
+//   .catch((error) => console.log(error));
+
+async function getAllData() {
+  const movies = await getData("./movies.json");
+  const actors = await getData("./actors.json");
+  const directors = await getData("./directors.json");
+  console.log(movies, actors, directors);
+}
+
+getAllData();
--- javascript/promises V callback hell/script.js
+++ javascript/promises V callback hell/script.js
@@ -1,11 +1,10 @@
 function getData(endpoint) {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
-
     xhr.open("GET", endpoint);
 
     xhr.onreadystatechange = function () {
-      if (xhr.onreadystatechange === 4) {
+      if (this.readyState === 4) {
         if (this.status === 200) {
           resolve(JSON.parse(this.responseText));
         } else {
@@ -19,14 +18,16 @@ function getData(endpoint) {
     }, Math.floor(Math.random() * 3000) + 1000);
   });
 }
-
-const moviesPromise = getData('./movies.json');
-const actorsPromise = getData("./actors.json");
-const directorsPromise = getData('./directors.json');
-
-Promise.all([moviesPromise, actorsPromise, directorsPromise])
-
-.then((data) => {
-  console.log(data);
-})
-.catch((error) => console.log(error))
+getData("./movies.json")
+  .then((movies) => {
+    console.log(movies);
+    return getData("./actors.json");
+  })
+  .then((actors) => {
+    console.log(actors);
+    return getData("./directors.json");
+  })
+  .then((directors) => {
+    console.log(directors);
+  })
+  .catch((error) => console.log(error));
